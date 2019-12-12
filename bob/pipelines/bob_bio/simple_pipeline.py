bc1256234cde3ada85876728b42097e9d283b671
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2019-12-12 16:58:32 +0100

    Using dask bags with the delays

--- bob/pipelines/bob_bio/simple_pipeline.py
+++ bob/pipelines/bob_bio/simple_pipeline.py
@@ -20,27 +20,27 @@ def split_data(input_data, n_groups):
     This is useful for paralellization
     """
 
-    offset=0
+    offset = 0
     # Number of elements per groups
-    step = len(input_data)//n_groups
+    step = len(input_data) // n_groups
 
     output_data = []
     for i in range(n_groups):
-        output_data.append(input_data[offset:offset+step])
+        output_data.append(input_data[offset : offset + step])
         offset += step
-    
 
     return output_data
 
 
-
-def pipeline(training_samples,
-             biometric_reference_samples,
-             probe_samples,
-             preprocessor,
-             extractor,
-             client,
-             experiment_path="my_experiment"):
+def pipeline(
+    training_samples,
+    biometric_reference_samples,
+    probe_samples,
+    preprocessor,
+    extractor,
+    client,
+    experiment_path="my_experiment",
+):
     """
     Create a simple pipeline for BIOMETRIC experiments.
 
@@ -66,14 +66,11 @@ def pipeline(training_samples,
     
     """
 
-
     ## SPLITING THE TASK IN N NODES
     n_nodes = len(client.cluster.workers)
     training_split = split_data(training_samples, n_nodes)
     biometric_reference_split = split_data(biometric_reference_samples, n_nodes)
     probe_split = split_data(probe_samples, n_nodes)
-
-
 
     # 1. PREPROCESSING
 
@@ -83,34 +80,27 @@ def pipeline(training_samples,
 
     # DECORATING FOR CACHING
     output_path = os.path.join(experiment_path, "./preprocessed")
-    #decorated_preprocess = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
-    decorated_preprocess = process_bobbio_samples # I'VE CHOSEN NOT TO CACHE THIS ONE
+    # decorated_preprocess = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
+    decorated_preprocess = process_bobbio_samples  # I'VE CHOSEN NOT TO CACHE THIS ONE
 
     for t_o, e_o, p_o in zip(training_split, biometric_reference_split, probe_split):
         preproc_training_futures.append(
-                client.submit(decorated_preprocess,
-                              biometric_samples=t_o,
-                              processor=preprocessor
-                             )
-                )
+            client.submit(
+                decorated_preprocess, biometric_samples=t_o, processor=preprocessor
+            )
+        )
 
-    
         preproc_enroll_futures.append(
-                client.submit(decorated_preprocess,
-                              biometric_samples=e_o,
-                              processor=preprocessor
-                             )
-                )
+            client.submit(
+                decorated_preprocess, biometric_samples=e_o, processor=preprocessor
+            )
+        )
 
-        
-          
         preproc_probe_futures.append(
-                client.submit(decorated_preprocess,
-                              biometric_samples=p_o,
-                              processor=preprocessor
-                             )
-                )
-    
+            client.submit(
+                decorated_preprocess, biometric_samples=p_o, processor=preprocessor
+            )
+        )
 
     # 2. EXTRACTION
 
@@ -121,47 +111,49 @@ def pipeline(training_samples,
     # DECORATING FOR CACHING
 
     output_path = os.path.join(experiment_path, "./extracted")
-    decorated_extractor = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
- 
-    for t_o, e_o, p_o in zip(preproc_training_futures, preproc_enroll_futures, preproc_probe_futures):
+    decorated_extractor = cache_bobbio_samples(output_path, ".hdf5")(
+        process_bobbio_samples
+    )
+
+    for t_o, e_o, p_o in zip(
+        preproc_training_futures, preproc_enroll_futures, preproc_probe_futures
+    ):
         extractor_training_futures.append(
-                client.submit(decorated_extractor,
-                              biometric_samples=t_o,
-                              processor=extractor
-                             )
-                )
+            client.submit(
+                decorated_extractor, biometric_samples=t_o, processor=extractor
+            )
+        )
 
         extractor_enroll_futures.append(
-                client.submit(decorated_extractor,
-                              biometric_samples=e_o,
-                              processor=extractor
-                             )
-                )
+            client.submit(
+                decorated_extractor, biometric_samples=e_o, processor=extractor
+            )
+        )
 
         extractor_probe_futures.append(
-                client.submit(decorated_extractor,
-                              biometric_samples=p_o,
-                              processor=extractor
-                             )
-                )
+            client.submit(
+                decorated_extractor, biometric_samples=p_o, processor=extractor
+            )
+        )
 
     # Dumping futures
-    for t_o, e_o, p_o in zip(extractor_training_futures, extractor_enroll_futures, extractor_probe_futures):
+    for t_o, e_o, p_o in zip(
+        extractor_training_futures, extractor_enroll_futures, extractor_probe_futures
+    ):
         t_o.result()
         e_o.result()
         p_o.result()
 
 
-
-
-
-def pipeline_DELAY(training_samples,
-             biometric_reference_samples,
-             probe_samples,
-             preprocessor,
-             extractor,
-             client,
-             experiment_path="my_experiment"):
+def pipeline_DELAY(
+    training_samples,
+    biometric_reference_samples,
+    probe_samples,
+    preprocessor,
+    extractor,
+    client,
+    experiment_path="my_experiment",
+):
     """
     Create a simple pipeline for BIOMETRIC experiments.
 
@@ -188,15 +180,26 @@ def pipeline_DELAY(training_samples,
     """
 
     import dask.delayed
-
+    import dask.bag
 
     ## SPLITING THE TASK IN N NODES
     n_nodes = len(client.cluster.workers)
-    training_split = split_data(training_samples, n_nodes)
-    biometric_reference_split = split_data(biometric_reference_samples, n_nodes)
-    probe_split = split_data(probe_samples, n_nodes)
 
+    # training_split = split_data(training_samples, n_nodes)
+    # biometric_reference_split = split_data(biometric_reference_samples, n_nodes)
+    # probe_split = isplit_data(probe_samples, n_nodes)
 
+    training_split = dask.bag.from_sequence(
+        training_samples, npartitions=n_nodes
+    ).to_delayed()
+
+    biometric_reference_split = dask.bag.from_sequence(
+        biometric_reference_samples, npartitions=n_nodes
+    ).to_delayed()
+
+    probe_split = dask.bag.from_sequence(
+        probe_samples, npartitions=n_nodes
+    ).to_delayed()
 
     # 1. PREPROCESSING
 
@@ -206,35 +209,29 @@ def pipeline_DELAY(training_samples,
 
     # DECORATING FOR CACHING
     output_path = os.path.join(experiment_path, "./preprocessed")
-    #decorated_preprocess = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
- 
-    decorated_preprocess = process_bobbio_samples # I'VE CHOSEN NOT TO CACHE THIS ONE
-    
+    # decorated_preprocess = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
+
+    decorated_preprocess = process_bobbio_samples  # I'VE CHOSEN NOT TO CACHE THIS ONE
+
     for t_o, e_o, p_o in zip(training_split, biometric_reference_split, probe_split):
         preproc_training_futures.append(
+            dask.delayed(decorated_preprocess)(
+                biometric_samples=t_o, processor=preprocessor
+            )
+        )
 
-                dask.delayed(decorated_preprocess)
-                            (biometric_samples=t_o,
-                            processor=preprocessor)
-                )
-                
-                
-    
         preproc_enroll_futures.append(
-                dask.delayed(decorated_preprocess)
-                             (biometric_samples=e_o,
-                              processor=preprocessor
-                             )
-                )
+            dask.delayed(decorated_preprocess)(
+                biometric_samples=e_o, processor=preprocessor
+            )
+        )
 
-        
-          
         preproc_probe_futures.append(
-                dask.delayed(decorated_preprocess)
-                            (biometric_samples=p_o,
-                             processor=preprocessor)
-                )
-   
+            dask.delayed(decorated_preprocess)(
+                biometric_samples=p_o, processor=preprocessor
+            )
+        )
+
     # 2. EXTRACTION
 
     extractor_training_futures = []
@@ -244,30 +241,35 @@ def pipeline_DELAY(training_samples,
     # DECORATING FOR CACHING
 
     output_path = os.path.join(experiment_path, "./extracted")
-    decorated_extractor = cache_bobbio_samples(output_path, ".hdf5")(process_bobbio_samples)
- 
-    for t_o, e_o, p_o in zip(preproc_training_futures, preproc_enroll_futures, preproc_probe_futures):
+    decorated_extractor = cache_bobbio_samples(output_path, ".hdf5")(
+        process_bobbio_samples
+    )
+
+    for t_o, e_o, p_o in zip(
+        preproc_training_futures, preproc_enroll_futures, preproc_probe_futures
+    ):
         extractor_training_futures.append(
-                dask.delayed(decorated_extractor)
-                            ( biometric_samples=t_o,
-                              processor=extractor)
-                )
+            dask.delayed(decorated_extractor)(
+                biometric_samples=t_o, processor=extractor
+            )
+        )
 
         extractor_enroll_futures.append(
-                dask.delayed(decorated_extractor)
-                             (biometric_samples=e_o,
-                              processor=extractor)
-                )
+            dask.delayed(decorated_extractor)(
+                biometric_samples=e_o, processor=extractor
+            )
+        )
 
         extractor_probe_futures.append(
-                dask.delayed(decorated_extractor)
-                             (biometric_samples=p_o,
-                              processor=extractor)
-                )
+            dask.delayed(decorated_extractor)(
+                biometric_samples=p_o, processor=extractor
+            )
+        )
 
     # Dumping futures
-    for t_o, e_o, p_o in zip(extractor_training_futures, extractor_enroll_futures, extractor_probe_futures):
+    for t_o, e_o, p_o in zip(
+        extractor_training_futures, extractor_enroll_futures, extractor_probe_futures
+    ):
         t_o.compute(scheduler=client)
         e_o.compute(scheduler=client)
         p_o.compute(scheduler=client)
-
