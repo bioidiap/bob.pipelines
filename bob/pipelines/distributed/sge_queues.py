0ad6764c1572f1d2eff24733d43fea55ed4b3eea
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-07-07 11:19:04 +0200

    Created a demmand queue

--- bob/pipelines/distributed/sge_queues.py
+++ bob/pipelines/distributed/sge_queues.py
@@ -12,9 +12,9 @@ QUEUE_DEFAULT = {
     "default": {
         "queue": "q_1day",
         "memory": "8GB",
-        "io_big": True,
+        "io_big": False,
         "resource_spec": "",
-        "max_jobs": 48,
+        "max_jobs": 96,
         "resources": "",
     },
     "q_1week": {
@@ -56,11 +56,65 @@ For CPU jobs, it prioritizes all.q and not io_big
 """
 QUEUE_LIGHT = {
     "default": {
-        "queue": "all.q",
+        "queue": "q_1day",
         "memory": "4GB",
         "io_big": False,
         "resource_spec": "",
         "max_jobs": 96,
+        "resources": "",
+    },
+    "q_1day": {
+        "queue": "q_1day",
+        "memory": "4GB",
+        "io_big": False,
+        "resource_spec": "",
+        "max_jobs": 48,
+        "resources": {"q_1day": 1},
+    },
+    "q_1week": {
+        "queue": "q_1week",
+        "memory": "4GB",
+        "io_big": True,
+        "resource_spec": "",
+        "resources": {"q_1week": 1},
+    },
+    "q_short_gpu": {
+        "queue": "q_short_gpu",
+        "memory": "30GB",
+        "io_big": False,
+        "resource_spec": "",
+        "max_jobs": 45,
+        "resources": {"q_short_gpu": 1},
+    },
+    "q_gpu": {
+        "queue": "q_gpu",
+        "memory": "30GB",
+        "io_big": False,
+        "resource_spec": "",
+        "resources": {"q_gpu": 1},
+    },
+    "q_long_gpu": {
+        "queue": "q_long_gpu",
+        "memory": "30GB",
+        "io_big": False,
+        "resource_spec": "",
+        "resources": {"q_long_gpu": 1},
+    },
+}
+
+
+
+"""
+This queue setup has a light arrangement.
+For CPU jobs, it prioritizes all.q and not io_big
+"""
+QUEUE_DEMANDING = {
+    "default": {
+        "queue": "q_1day",
+        "memory": "8GB",
+        "io_big": True,
+        "resource_spec": "",
+        "max_jobs": 48,
         "resources": "",
     },
     "q_1day": {
