1bba35ade6f80ff8ebb1dece4a7c9b13479ee48e
Yannick DAYER <yannick.dayer@idiap.ch>
2020-10-12 16:19:50 +0200

    [setup] Add entry points for dask client config
    Requires a fix in bob.bio.base: see bob.bio.base!201

--- bob/pipelines/config/distributed/sge_demanding.py
+++
@@ -1,7 +0,0 @@
-from dask.distributed import Client
-
-from bob.pipelines.distributed.sge import SGEMultipleQueuesCluster
-from bob.pipelines.distributed.sge_queues import QUEUE_DEMANDING
-
-cluster = SGEMultipleQueuesCluster(min_jobs=20, sge_job_spec=QUEUE_DEMANDING)
-dask_client = Client(cluster)
