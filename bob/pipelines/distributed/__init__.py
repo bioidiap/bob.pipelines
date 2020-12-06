fde28dfc443d24c78fc2265cd663b8ebcc727acd
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-12-06 19:20:55 +0100

    moved VALID_DASK_CLIENT_STRINGS to bob.pipelines

--- bob/pipelines/distributed/__init__.py
+++ bob/pipelines/distributed/__init__.py
@@ -2,3 +2,17 @@
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
+
+# DASK-click VALID_DASK_CLIENT_STRINGS
+# TO BE USED IN:
+# @click.option(
+#    "--dask-client",
+#    "-l",
+#    entry_point_group="dask.client",
+#    string_exceptions=VALID_DASK_CLIENT_STRINGS,
+#    default="single-threaded",
+#    help="Dask client for the execution of the pipeline.",
+#    cls=ResourceOption,
+# )
+
+VALID_DASK_CLIENT_STRINGS = ("single-threaded", "sync", "threaded", "processes")
