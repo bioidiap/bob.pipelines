72ca283d21810260ceaaa89da5f701a238c59ce4
Hatef OTROSHI <hatef.otroshi@idiap.ch>
2021-08-20 14:25:03 +0200

    + breakdown_SampleSet

--- bob/pipelines/tests/test_utils.py
+++ bob/pipelines/tests/test_utils.py
@@ -1,4 +1,12 @@
+import random
+
+import numpy as np
+
 import bob.pipelines as mario
+
+from bob.pipelines import Sample
+from bob.pipelines import SampleSet
+from bob.pipelines.utils import flatten_samplesets
 
 
 def test_isinstance_nested():
@@ -22,3 +30,39 @@ def test_isinstance_nested():
     assert mario.utils.isinstance_nested(o, "o", C)
     assert mario.utils.isinstance_nested(o, "o", B)
     assert not mario.utils.isinstance_nested(o, "o", A)
+
+
+def test_break_sample_set():
+
+    samplesets = []
+    n_samples = 10
+    X = np.ones(shape=(n_samples, 2), dtype=int)
+    random.seed(10)
+
+    # Creating a face list of samplesets
+    for i in range(n_samples):
+
+        samplesets.append(
+            SampleSet(
+                [
+                    Sample(
+                        data,
+                        key=str(i),
+                        sample_random_attriute="".join(
+                            [random.choice("abcde") for _ in range(5)]
+                        ),
+                    )
+                    for i, data in enumerate(X)
+                ],
+                key=str(i),
+                sampleset_random_attriute="".join(
+                    [random.choice("abcde") for _ in range(5)]
+                ),
+            )
+        )
+
+    # Flatting the SSET
+    new_samplesets = flatten_samplesets(samplesets)
+
+    assert len(new_samplesets) == n_samples * n_samples
+    assert np.allclose([len(s) for s in new_samplesets], np.ones(n_samples * n_samples))
